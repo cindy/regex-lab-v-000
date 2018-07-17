ef1f5bf8610f8e8c89de0c60ea96752da2970823
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-07-17 03:36:33 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ end
 
 def words_five_letters_long(text)
   puts text
-  puts text.scan(/\w{5}\S/)
+  puts text.scan(/\w{5}\S/).to_s
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
