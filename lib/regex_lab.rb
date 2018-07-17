823c56297d0eb869f9127d2b407c63852a665953
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-07-17 03:49:12 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\D?\d{3}\D?\d{3}\D?\d{4}/) 
+  phone.scan(/\D?\d{3}\D?\d{3}\D?\d{4}/) != []
 end
 
 # match = %w{ afoot Excellent incredible Absolute unreal Inconceivable octopus }
