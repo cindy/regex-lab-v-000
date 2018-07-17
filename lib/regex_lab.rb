663a97c8cee2b69138a5503031acfffa23ec4b07
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-07-17 03:45:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  puts text.scan(/[A-Z]\w\w+[!?.,;:]/)
+  text.scan(/[A-Z]\w\w+[!?.,;:]/) != []
 end
 
 def valid_phone_number?(phone)
