15605d29db5f0f251281a1276a86bbf0812eaebf
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-07-17 03:34:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/^[aeiouAEIOU]\w?/) != []
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/[unUN]{2}\w*[INGing]{3}/)
 end
 
 def words_five_letters_long(text)
-
+  text.grep(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -17,3 +17,10 @@ end
 def valid_phone_number?(phone)
 
 end
+
+# match = %w{ afoot Excellent incredible Absolute unreal Inconceivable octopus }
+#
+# match.each do |word|
+#   starts_with_a_vowel?(word)
+# end
+# words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
